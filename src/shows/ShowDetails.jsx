fdd760231fd2343258656363a7abb64be1578c81
John Lawson <lawsonjohnr@gmail.com>
2026-07-02 12:35:22 -0500

    Still working on props

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,25 @@
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
+import { useState } from "react";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) {
+  const [selectedEpisode, setselectedEpisode] = useState()
+  console.log(show)
+  if(!show){
+    return <p>Select show for details</p>;
+  }
+
+  return(
+     <div className="show-details">
+        <EpisodeList
+        name = {show.name}
+        episodes = {show.episodes}
+        selectedEpisode = {selectedEpisode}
+        setSelectedEpisode = {setselectedEpisode} />
+        <EpisodeDetails
+        episode={selectedEpisode} />
+      </div>
+  );
 }
